f8b6dbc2814403471abcb9879ca0ebdecc50b3d3
Kunal Gosrani <kunalgosrani10@gmail.com>
2025-11-11 00:35:46 +0530

    feat(frontend): make design polish

--- backend/src/configs.ts
+++ backend/src/configs.ts
@@ -7,15 +7,15 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 const configs = {
-  port: Number(process.env.PORT) || 4000,
-  corsOrigin: process.env.CORS_ORIGIN || "http://localhost:3000",
-  dbHost: process.env.DB_HOST || "localhost",
-  dbPort: Number(process.env.DB_PORT) || 5432,
-  dbUser: process.env.DB_USER || "postgres",
-  dbPassword: process.env.DB_PASSWORD || "postgres",
-  dbName: process.env.DB_NAME || "rate-limiter",
+	port: Number(process.env.PORT) || 4000,
+	corsOrigin: process.env.CORS_ORIGIN || "http://localhost:3000",
+	dbHost: process.env.DB_HOST || "localhost",
+	dbPort: Number(process.env.DB_PORT) || 5432,
+	dbUser: process.env.DB_USER || "postgres",
+	dbPassword: process.env.DB_PASSWORD || "postgres",
+	dbName: process.env.DB_NAME || "rate-limiter",
 };
 
 export function getConfigs() {
-  return configs;
+	return configs;
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,15 +11,17 @@ export default function Home() {
 	const { data, errorMessage, isLoading, fetch } = useFetchRateLimiterData();
 
 	return (
-		<div className="container mx-auto max-w-3xl px-4 py-2">
-			<h1 className="text-2xl font-bold">Rate Limiter</h1>
-			<Button onClick={fetch}>Click me</Button>
+		<div className="px-4 py-2 bg-teal-700 min-h-screen">
+			<div className="max-w-md mx-auto bg-white rounded-2xl p-6 mt-48  text-center flex flex-col gap-4">
+				<h1 className="text-3xl font-bold uppercase">Rate Limiter</h1>
+				<Button onClick={fetch}>Click to refresh quota</Button>
 
-			<RateLimiterDataView
-				data={data}
-				isLoading={isLoading}
-				errorMessage={errorMessage}
-			/>
+				<RateLimiterDataView
+					data={data}
+					isLoading={isLoading}
+					errorMessage={errorMessage}
+				/>
+			</div>
 		</div>
 	);
 }
@@ -33,7 +35,7 @@ const RateLimiterDataView = ({
 	isLoading: boolean;
 	errorMessage: string | null;
 }) => {
-	if (isLoading) {
+	if (isLoading && !data) {
 		return <p className="text-gray-500">Loading...</p>;
 	}
 
@@ -47,12 +49,27 @@ const RateLimiterDataView = ({
 
 	if (!data.allowed) {
 		return (
-			<p className="text-red-500">
-				<AlertCircle className="w-4 h-4" />
-				You have exceeded your remaining quota. Please try again later.
-			</p>
+			<div className="flex flex-col justify-center gap-2">
+				<div className="p-4 px-8 rounded-xl mx-auto text-red-600 flex flex-col gap-2 bg-red-100">
+					<div className="flex items-center justify-center">
+						<AlertCircle className="w-12 h-12" />
+					</div>
+					<p className="">
+						You have exceeded your remaining quota.
+						<br />
+						Please try after some time.
+					</p>
+				</div>
+			</div>
 		);
 	}
 
-	return <p className="text-green-500">Remaining Quota: {data.remaining}</p>;
+	return (
+		<div className="flex flex-col justify-center gap-2">
+			<div className="border p-4 rounded-xl w-56 mx-auto">
+				<p className="text-lg">Your Remaining Quota</p>
+				<p className="text-5xl font-bold">{data.remaining}</p>
+			</div>
+		</div>
+	);
 };
