bbd7b62abe2633faffda8d4f55b7d98648768801
Kunal Gosrani <kunalgosrani10@gmail.com>
2025-11-10 19:27:16 +0530

    fix(backend): request ip-address extraction

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,37 +4,39 @@ import { cors } from "hono/cors";
 import { getConfigs } from "./configs";
 import { migrateDb } from "./db";
 import { getRemainingTokens } from "./services/get-remaining-tokens";
+import { getConnInfo } from "@hono/node-server/conninfo";
 
 const configs = getConfigs();
 
 async function main() {
-	await migrateDb();
+  await migrateDb();
 
-	const app = new Hono();
+  const app = new Hono();
 
-	app.use(
-		"/*",
-		cors({ origin: configs.corsOrigin, allowMethods: ["GET", "OPTIONS"] }),
-	);
+  app.use(
+    "/*",
+    cors({ origin: configs.corsOrigin, allowMethods: ["GET", "OPTIONS"] })
+  );
 
-	app.get("/api/ping", async (c) => {
-		const ipAddress = c.req.header("X-Forwarded-For") ?? "";
+  app.get("/api/ping", async (c) => {
+    const info = getConnInfo(c);
+    const ipAddress = info.remote.address as string;
 
-		const remainingTokens = await getRemainingTokens(ipAddress);
+    const remainingTokens = await getRemainingTokens(ipAddress);
 
-		if (remainingTokens <= 0) {
-			return c.json({ allowed: false, remaining: remainingTokens }, 429);
-		}
+    if (remainingTokens <= 0) {
+      return c.json({ allowed: false, remaining: remainingTokens }, 429);
+    }
 
-		return c.json({ allowed: true, remaining: remainingTokens });
-	});
+    return c.json({ allowed: true, remaining: remainingTokens });
+  });
 
-	serve({ fetch: app.fetch, port: configs.port }, (info) => {
-		console.log(`Server is running on http://localhost:${info.port}`);
-	});
+  serve({ fetch: app.fetch, port: configs.port }, (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  });
 }
 
 main().catch((error) => {
-	console.error("Error starting server:", error);
-	process.exit(1);
+  console.error("Error starting server:", error);
+  process.exit(1);
 });
